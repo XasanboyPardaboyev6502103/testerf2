e3109620624af33139664d7975aebe9351fba67a
XasanboyPardaboyev6502103 <yeie4225@gmail.com>
2026-06-05 12:43:32 +0500

    adfvdf

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
-# import asyncio
+# from aiogram import Bot,Dispatcher
+from aiogram import F
+from asyncio import run
+from aiogram.filters import Command
+from aiogram.types import Message,FSInputFile
+from funksiyalarim import ovoz_yarat,keyboard_btn
+import os
 
-# loop = asyncio.new_event_loop()
-# asyncio.set_event_loop(loop)
+TOKEN="5813418126:AAEzkw5uxtjZFg6Hdrsu_BNe8cqLFG_ZzzE"
 
-# from pyrogram import Client, filters
-
-
+bot=Bot(TOKEN)
+dp=Dispatcher()
 
 
-# API_ID = 33243686
-# API_HASH = "0b4532fb6f2ac9dd03b0038f1a7b75e3"
+@dp.message(Command("start"))
+async def start(message:Message):
+    await bot.send_message(chat_id="@gruh01",text="salomlar gruppadoshlar")
+    await message.answer(f"Assalomu alaykum {message.from_user.full_name}",reply_markup=keyboard_btn)
 
-# KATTA = "@gruh01"
-# KICHIK = "@yoshlikdagiEngZurEskiMultfilmla"
 
-# KALIT_SUZ=[
-#     "zakaz","taksi kerak","taksi"
-# ]
 
-# app = Client("mening_sessiyam", api_id=API_ID, api_hash=API_HASH)
+@dp.message(F.contact)
+async def nomer(msg:Message):
+    phone=msg.contact.phone_number
+    await msg.answer(f"sizning nomeringiz {phone}")
 
-# @app.on_message(filters.chat(KATTA))
-# async def forward_handler(client, message):
-#     txt=message.text.lower()
-#     for t in KALIT_SUZ:
-#         if txt in t:
-#             await message.forward(KICHIK)
-#             await message.delete()
-#             print("belgilangan xabar uchirildi!")
-            
-    
-#     print("Yuborildi")
+# msg.answer("salom")
+# msg.repl("salom")
+# bot.send(52345324,"salom")
 
-# app.run()
+@dp.message(F.location)
+async def nomer(msg:Message):
+    lat=msg.location.latitude
+    lon=msg.location.longitude
+    await msg.answer(f"sizning joylashuvingiz long: {lon}, lat: {lat}")
+
+@dp.message()
+async def others(message:Message):
+    text=message.text
+    if text=="salom":
+        await message.answer("Va alaykum salom")
+    elif text=="Ikki":
+        await message.answer("sen ikkini bosding")
+    else:
+        fayl_nomi=f"{message.from_user.username}_{message.text}.mp3"
+        await ovoz_yarat(message.text,fayl_nomi)
+        ovozim=FSInputFile(fayl_nomi)
+        await message.answer_audio(audio=ovozim,caption=message.text)
+        if os.path.exists(fayl_nomi):
+            os.remove(fayl_nomi)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+run(main())
